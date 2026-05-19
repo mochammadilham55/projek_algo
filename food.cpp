4bbee6f76f12e0b704966138b2f32a6f13de3798
Mochammad Ilham <mochammadilhamrizki1@gmail.com>
2026-05-19 13:28:37 +0700

    penambahan struct data makanan

--- food.cpp
+++ food.cpp
@@ -1,5 +1,12 @@
 #include <iostream>
-
+#include <string>
+#include <queue>
 using namespace std;
 
+struct Food {
+    string nama;
+    int porsi;      // Value dalam knapsack
+    int berat;      // Weight dalam knapsack
+    int expiry;     // Prioritas (semakin kecil, semakin urgent)
+};
 
