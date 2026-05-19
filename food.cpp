969eedc9745fe3144d90ca4b4e6f646babebb035
mochammadilham55 <mochammadilhamrizki1@gmail.com>
2026-05-19 14:57:37 +0700

    Clarify comments in Food
    Updated comments for clarity on Food struct members.

--- food.cpp
+++ food.cpp
@@ -5,8 +5,8 @@ using namespace std;
 
 struct Food {       // untuk menyimpan data makanan
     string nama;
-    int porsi;      // Value dalam knapsack
-    int berat;      // Weight dalam knapsack
+    int porsi;      // menjadi Value dalam knapsack
+    int berat;      // Akan menjadi Weight dalam knapsack
     int expiry;     // Prioritas (semakin kecil, semakin urgent)
 };
 
